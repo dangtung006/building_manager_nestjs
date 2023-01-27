9ecb972e17dd6925cfaa55717fb0020842467ad1
dangtung <language0512@gmail.com>
2023-01-27 16:49:38 +0700

    update filter request

---
+++ src/common/filters/httpException.filter.ts
@@ -0,0 +1,22 @@
+import { 
+    ArgumentsHost, 
+    ExceptionFilter,
+    Catch,
+    HttpException
+} from "@nestjs/common";
+
+import { Request, Response } from 'express';
+
+Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter{
+    catch(exception: HttpException, host: ArgumentsHost) {
+        const context = host.switchToHttp();
+        const request = context.getRequest<Request>();
+        const response = context.getResponse<Response>();
+
+        return  response.send({
+            status : exception.getStatus(),
+            message : exception.getResponse()
+        })
+    }
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Post, Body, Put, Param, Get, Delete } from '@nestjs/common';
+import { Controller, Post, Body, Put, Param, Get, Delete, UseFilters } from '@nestjs/common';
 import { plainToInstance } from 'class-transformer';
 import { UserDto } from './user.dto';
 import { UserService } from './user.service';
+import { HttpExceptionFilter } from './../common/filters/httpException.filter';
 
 @Controller('user')
 
@@ -20,7 +21,8 @@ export class UserController {
     update(@Param('id') id : string, @Body() user : UserDto):Promise<{result : number}>{
         return this.userService.updateById(id, user);
     }
-
+    
+    @UseFilters(HttpExceptionFilter)
     @Get(':id')
     getDetail(@Param('id') id : string):Promise<UserDto>{
         let user = this.userService.getById(id);
