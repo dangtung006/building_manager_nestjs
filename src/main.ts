6aa58249140c48628cb65f2b14cff980f364dbe6
dangtung <language0512@gmail.com>
2023-01-31 11:19:59 +0700

    update view engine and static file

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as session from 'express-session';
 import * as passport from 'passport';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
     app.useGlobalPipes(new ValidationPipe());
-
     app.use(
         session({
             secret: 'my-secret',
@@ -15,9 +16,13 @@ async function bootstrap() {
             saveUninitialized: false,
         })
     );
-
     app.use(passport.initialize());
     app.use(passport.session());
+
+    app.useStaticAssets(join(__dirname, '..', 'public'));
+    app.setBaseViewsDir(join(__dirname, '..', 'views'));
+    app.setViewEngine('ejs');
+
     await app.listen(3000);
 }
 
